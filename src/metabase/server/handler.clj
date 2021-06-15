8d36cf355a4ce49037a47de0e17b48d487156a3e
Guillermo Ríos <grios@stratio.com>
2021-06-15 19:09:58 +0200

    [ROCK-4767] Fix: Automatic auto-login with expired sessions (#277)
    * Auto-login when no user info found
    
    * Update changelog

--- src/metabase/server/handler.clj
+++ src/metabase/server/handler.clj
@@ -61,10 +61,10 @@
    #'mw.misc/maybe-set-site-url                 ; set the value of `site-url` if it hasn't been set yet
    #'mw.session/reset-session-timeout           ; Resets the timeout cookie for user activity to [[mw.session/session-timeout]]
    #'mw.session/bind-current-user               ; Binds *current-user* and *current-user-id* if :metabase-user-id is non-nil
-   #'mw.session/wrap-current-user-info          ; looks for :metabase-session-id and sets :metabase-user-id and other info if Session ID is valid
    ;; < STRATIO - add custom middleware
    #'st.mw/stratio-middleware                ; auto-login, forbid email login, add username in response header...
    ;; STRATIO />
+   #'mw.session/wrap-current-user-info          ; looks for :metabase-session-id and sets :metabase-user-id and other info if Session ID is valid
    #'mw.session/wrap-session-id                 ; looks for a Metabase Session ID and assoc as :metabase-session-id
    #'mw.auth/wrap-static-api-key                ; looks for a static Metabase API Key on the request and assocs as :metabase-api-key
    #'wrap-cookies                               ; Parses cookies in the request map and assocs as :cookies
